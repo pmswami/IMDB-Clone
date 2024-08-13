d84374968c74ae0f05145abcbb0db02397055555
swamfire <usern5949@gmail.com>
2024-08-13 12:40:34 +0530

    Added AppBar

---
+++ imdb-clone/src/components/common/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { AppBar, Toolbar } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+
+function Header() {
+  return (
+    <>
+      <AppBar>
+        <Toolbar></Toolbar>
+      </AppBar>
+    </>
+  );
+}
+
+export default Header;
--- imdb-clone/src/pages/Home.jsx
+++ imdb-clone/src/pages/Home.jsx
@@ -1,5 +1,11 @@
+import Header from "../components/common/Header";
+
 const Home = () => {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default Home;
